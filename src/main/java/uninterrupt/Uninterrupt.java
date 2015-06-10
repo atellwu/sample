965e4b267202139ab7e2afefb1fbe14f3667ddb6
atell.wu <atell.wu@ndpmedia.com>
2015-06-10 12:03:42 +0800

    dasf

--- src/main/java/uninterrupt/Uninterrupt.java
+++ src/main/java/uninterrupt/Uninterrupt.java
@@ -7,7 +7,7 @@ import java.util.concurrent.locks.ReentrantLock;
 import com.google.common.util.concurrent.FutureCallback;
 
 // awaitUninterruptibly是可以被kill <pid>以及 kill -9 <pid>的，这和操作系统进程真正的不可中断状态不一样。
-// addShutdownHook是在kill <pid>触发的，kill -3只是触发打印线程状态。
+// addShutdownHook是在kill <pid>触发的，kill -3只是触发打印线程和堆状态。
 public class Uninterrupt {
 
 	public static void main(String[] args) {
